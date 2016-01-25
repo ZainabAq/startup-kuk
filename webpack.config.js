29095dab7faf7eb1a389f6f33bb27c640f36a83a
John Vilk <jvilk@cs.umass.edu>
2016-01-25 15:39:02 -0500

    Updating webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+var path = require('path');
+
 module.exports = {
   // The main "entry point" of your web app. WebPack will pack every module that
   // this file depends on (and its dependencies depend on).
   entry: './app/app.js',
   // Package up the application as 'app.js' in the 'build/js' directory.
+  // __dirname is a magic Node.js variable that points to the directory of
+  // webpack.config.js itself.
   output: {
-    filename: './build/js/app.js'
+    filename: path.resolve(__dirname, './build/js/app.js')
   },
   module: {
     // Transforms your application's code using Babel.
@@ -15,9 +19,16 @@ module.exports = {
     // (The babel-loader will also compile your React templates to JavaScript.)
     loaders: [
       {
+        // Only transform *.js files.
         test: /\.js$/,
+        // Don't transform any of the modules you depend on -- just transform
+        // *your* code.
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
+          // es2015 == ECMAScript 2015. Lets you use new JavaScript features.
+          // The React module runs the React compiler, which translates the
+          // HTML in your React modules into code.
           presets: ['es2015', 'react']
         }
       }
