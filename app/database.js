9eb176fb0f017ac2d4d32ab2143e6755ae663a7d
Stephanie Xie <sxie@umass.edu>
2016-03-05 14:27:07 -0500

    Mock Database & Server

---
+++ app/database.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Modify with your startup's name!
+var kuk = null;
+
+// Put your mock objects here, as in Workshop 4
+var initialData = {
+
+};
+
+var data = JSON.parse(localStorage.getItem(kuk));
+if (data === null) {
+  data = JSONClone(initialData);
+}
+
+/**
+ * A dumb cloning routing. Serializes a JSON object as a string, then
+ * deserializes it.
+ */
+function JSONClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+/**
+ * Emulates reading a "document" from a NoSQL database.
+ * Doesn't do any tricky document joins, as we will cover that in the latter
+ * half of the course. :)
+ */
+export function readDocument(collection, id) {
+  // Clone the data. We do this to model a database, where you receive a
+  // *copy* of an object and not the object itself.
+  return JSONClone(data[collection][id]);
+}
+
+/**
+ * Emulates writing a "document" to a NoSQL database.
+ */
+export function writeDocument(collection, changedDocument) {
+  var id = changedDocument._id;
+  // Store a copy of the object into the database. Models a database's behavior.
+  data[collection][id] = JSONClone(changedDocument);
+  // Update our 'database'.
+  localStorage.setItem(kuk, JSON.stringify(data));
+}
+
+/**
+ * Adds a new document to the NoSQL database.
+ */
+export function addDocument(collectionName, newDoc) {
+  var collection = data[collectionName];
+  var nextId = Object.keys(collection).length;
+  while (collection[nextId]) {
+    nextId++;
+  }
+  newDoc._id = nextId;
+  writeDocument(collectionName, newDoc);
+  return newDoc;
+}
+
+/**
+ * Reset our browser-local database.
+ */
+export function resetDatabase() {
+  localStorage.setItem(kuk, JSON.stringify(initialData));
+  data = JSONClone(initialData);
+}
+
+/**
+ * Reset database button.
+ */
+class ResetDatabase extends React.Component {
+  render() {
+    return (
+      <button className="btn btn-default" type="button" onClick={() => {
+        resetDatabase();
+        window.alert("Database reset! Refreshing the page now...");
+        document.location.reload(false);
+      }}>Reset Mock DB</button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ResetDatabase />,
+  document.getElementById('db-reset')
+);
---
+++ app/server.js
@@ -0,0 +1,11 @@
+import {readDocument, writeDocument, addDocument} from './database.js';
+
+/**
+ * Emulates how a REST call is *asynchronous* -- it calls your function back
+ * some time in the future with data.
+ */
+function emulateServerReturn(data, cb) {
+  setTimeout(() => {
+    cb(data);
+  }, 4);
+}
