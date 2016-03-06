b169e32bdd82e20ec724260ac41a7f5b55155041
Farida Sabry <fsabry@smith.edu>
2016-03-06 11:04:24 -0500

    added browseFeed.js

---
+++ app/components/browseFeed.js
@@ -0,0 +1,168 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class BrowseFeed extends React.Componenet {
+  render() {
+    return (
+      <div>
+        <h1 className="center ">Browse Our Recipes</h1>
+        <ul id="categories" className="clr">
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/432846.jpg" alt=""/>
+                <h1>Baked Chicken</h1>
+                <p>Roast chicken pieces with sticky, sweet and tangy coating
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+            <li>
+              <div>
+                <img src="http://dish.allrecipes.com/wp-content/uploads/2015/04/Poached-Eggs_Eggs-Benedict_Photo-by-Meredith.jpg" alt=""/>
+                <h1>Bagels</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/2964964.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="img/logo2.png" className="special-logo" alt=""/>
+                <h1>Have a delicious day!</h1>
+                <p>Love from the Kuk team</p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/1655056.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/3246513.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li className="pusher"></li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/672001.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/2343032.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/3118122.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/3088926.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li><li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/2333962.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/861374.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/3213063.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li className="pusher"></li>
+          <li className="pusher"></li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/842371.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li className="pusher"></li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/738814.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/2728664.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+          <li>
+              <div>
+                <img src="http://images.media-allrecipes.com/userphotos/600x600/1523484.jpg" alt=""/>
+                <h1>This is a recipe</h1>
+                <p>Some sample text about the recipe this hexagon leads to
+                  <i className="glyphicon glyphicon-time info pull-left">15 min</i>
+                </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
