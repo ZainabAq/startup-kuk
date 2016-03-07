224af514b548b719d3e2f3f929047cb7780ac842
Eleanor Ewing <eewing@smith.edu>
2016-03-07 10:20:01 -0500

    Fixing bootlint errors for profile

--- app/components/profileRestrictions.js
+++ app/components/profileRestrictions.js
@@ -8,62 +8,64 @@ export default class ProfileRestrictions extends React.Component {
         <div className="panel-body font1">
           <form role="form">
             <div className="row">
-              <div className="col-xs-10 col-xs-offset-1 row">
-                <div className="col-xs-6">
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Dairy
-                    </label>
+              <div className="col-xs-10 col-xs-offset-1">
+                <div className="row">
+                  <div className="col-xs-6">
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Dairy
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Eggs
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Nuts
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Soy
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Gluten
+                      </label>
+                    </div>
                   </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Eggs
-                    </label>
+                  <div className="col-xs-6">
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Fish
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Shellfish
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Poultry
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Beef
+                      </label>
+                    </div>
+                    <div className="checkbox">
+                      <label>
+                        <input type="checkbox" />Pork
+                      </label>
+                    </div>
                   </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Nuts
-                    </label>
-                  </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Soy
-                    </label>
-                  </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Gluten
-                    </label>
-                  </div>
-                </div>
-                <div className="col-xs-6">
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Fish
-                    </label>
-                  </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Shellfish
-                    </label>
-                  </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Poultry
-                    </label>
-                  </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Beef
-                    </label>
-                  </div>
-                  <div className="checkbox">
-                    <label>
-                      <input type="checkbox" />Pork
-                    </label>
-                  </div>
-                </div>
               </div>
+            </div>
             </div>
             <hr />
             <button type="submit" className="btn btn-default pull-right">Save</button>
