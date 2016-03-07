22709c3833d6041ac761afc4d389efb922fd7be9
Eleanor Ewing <eewing@smith.edu>
2016-03-07 12:54:42 -0500

    Setting up temporary routing for testing, adding css references to index.html

--- app/app.js
+++ app/app.js
@@ -1,16 +1,20 @@
 /* Empty for now. :) */
 import React from "react";
 import ReactDOM from "react-dom";
-import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 import BrowseFeed from './components/browseFeed';
 import Recipe from './components/recipe';
+import ProfileFeed from './components/profileFeed';
+import ResultsFeed from './components/resultsFeed';
+import Instamode from './components/instamode';
+import Favorites from './components/favorites';
+import Calendar from './components/calendar';
 
 /**
  * A recipe page
  */
 class RecipePage extends React.Component {
   render() {
-     console.log("In the recipePage render function");
     return <Recipe />
   }
 }
@@ -20,8 +24,52 @@ class RecipePage extends React.Component {
  */
 class BrowsePage extends React.Component {
   render() {
-     console.log("In the BrowsePage render")
     return <BrowseFeed />
+  }
+}
+
+/**
+ * The profile page
+ */
+class ProfilePage extends React.Component {
+  render() {
+    return <ProfileFeed />
+  }
+}
+
+/**
+ * The results page
+ */
+class ResultsPage extends React.Component {
+  render() {
+    return <ResultsFeed />
+  }
+}
+
+/**
+ * The instamode page
+ */
+class InstaPage extends React.Component {
+  render() {
+    return <Instamode />
+  }
+}
+
+/**
+ * Favorites page
+ */
+class FavoritesPage extends React.Component {
+  render() {
+    return <Favorites />
+  }
+}
+
+/**
+ * Calendar page
+ */
+class CalendarPage extends React.Component {
+  render() {
+    return <Calendar />
   }
 }
 
@@ -34,10 +82,15 @@ class App extends React.Component {
 }
 
 ReactDOM.render((
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={BrowsePage} />
       <Route path="recipe/:id" component={RecipePage} />
+      <Route path="profile/:id" component={ProfilePage} />
+      <Route path="results" component={ResultsPage} />
+      <Route path="instamode" component={InstaPage} />
+      <Route path="favorites/:id" component={FavoritesPage} />
+      <Route path="calendar/:id" component={CalendarPage} />
     </Route>
   </Router>
 ),document.getElementById('main-page'));
