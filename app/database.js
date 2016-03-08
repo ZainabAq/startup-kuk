1fb586192d03b9fb9ac8ed9f5a5a1765349f62a7
Eleanor Ewing <eewing@smith.edu>
2016-03-08 16:41:00 -0500

    Adding reset database button (bottom of the page)

--- app/database.js
+++ app/database.js
@@ -160,22 +160,22 @@ export function resetDatabase() {
   data = JSONClone(initialData);
 }
 
-// /**
-// * Reset database button.
-// */
-// class ResetDatabase extends React.Component {
-//   render() {
-//     return (
-//       <button className="btn btn-default" type="button" onClick={() => {
-//           resetDatabase();
-//           window.alert("Database reset! Refreshing the page now...");
-//           document.location.reload(false);
-//         }}>Reset Mock DB</button>
-//       );
-//     }
-//   }
-//
-//   ReactDOM.render(
-//     <ResetDatabase />,
-//     document.getElementById('db-reset')
-//   );
+/**
+* Reset database button.
+*/
+class ResetDatabase extends React.Component {
+  render() {
+    return (
+      <button className="btn btn-default" type="button" onClick={() => {
+          resetDatabase();
+          window.alert("Database reset! Refreshing the page now...");
+          document.location.reload(false);
+        }}>Reset Mock DB</button>
+      );
+    }
+  }
+
+  ReactDOM.render(
+    <ResetDatabase />,
+    document.getElementById('db-reset')
+  );
