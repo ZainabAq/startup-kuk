6dd498be40d28eac16a66346f18e846a84f8f3d7
Farida Sabry <fsabry@smith.edu>
2016-03-07 21:15:47 -0500

    Farida solved issues

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-/* Empty for now. :) */
 import React from "react";
 import ReactDOM from "react-dom";
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
@@ -104,33 +103,3 @@ ReactDOM.render((
 //    <BrowseFeed />,
 //    document.getElementById('main-page')
 // );
-
-
-// $(document).ready(function () {
-//   var trigger = $('.hamburger'),
-//       overlay = $('.overlay'),
-//      isClosed = false;
-//
-//     trigger.click(function () {
-//       hamburger_cross();
-//     });
-//
-//     function hamburger_cross() {
-//
-//       if (isClosed == true) {
-//         overlay.hide();
-//         trigger.removeClass('is-open');
-//         trigger.addClass('is-closed');
-//         isClosed = false;
-//       } else {
-//         overlay.show();
-//         trigger.removeClass('is-closed');
-//         trigger.addClass('is-open');
-//         isClosed = true;
-//       }
-//   }
-//
-//   $('[data-toggle="offcanvas"]').click(function () {
-//         $('#wrapper').toggleClass('toggled');
-//   });
-// });
