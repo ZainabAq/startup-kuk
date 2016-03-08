35c212c3ae40d5468b1cfda7afe12a50b10d8b5a
Zainab Aqdas <srizvi@smith.edu>
2016-03-08 16:58:01 -0500

    Added calendarentry React Component

--- app/components/calendar.js
+++ app/components/calendar.js
@@ -1,6 +1,37 @@
 import React from 'react';
+import CalendarEntry from './calendarentry';
+import {getCalendarSync} from '../server';
 
 export default class Calendar extends React.Component {
+    constructor(props) {
+      this.state = {
+        meals = []
+      };
+    }
+
+    refresh() {
+      getCalendarSync(this.props.user, (calendarData) => {
+        this.setState(calendarData)
+      });
+    }
+
+    onEdit() {
+
+    }
+
+    onPreviousWeek() {
+
+    }
+
+    onNextWeek() {
+
+    }
+
+    componentsDidMount() {
+      this.refresh();
+    }
+
+
   render() {
     return(
       <div>
@@ -26,51 +57,17 @@ export default class Calendar extends React.Component {
                   <th>Sunday</th>
                 </tr>
               </thead>
-              <tbody>
-                <tr>
-                  <td className="heading"><strong>Breakfast</strong></td>
-                  <td>Scrambled tofu<center><img src="img/planning-images/scrambled-tofu.jpg" className="img-rounded" /></center></td>
-                  <td>Oatmeal with fruit<center><img src="img/planning-images/oatmeal-fruit.jpg" className="img-rounded" /></center></td>
-                  <td>Eggs and bacon<center><img src="img/planning-images/eggs-bacon.jpg" className="img-rounded" /></center></td>
-                  <td>Banana bread<center><img src="img/planning-images/banana-bread.jpg" className="img-rounded" /></center></td>
-                  <td>Smoothie and bagel<center><img src="img/planning-images/bagel-smoothie.jpg" className="img-rounded" /></center></td>
-                  <td>Pb&j sandwich<center><img src="img/planning-images/peanut-jelly.jpg" className="img-rounded" /></center></td>
-                  <td>Yogurt and fruit<center><img src="img/planning-images/yogurt-fruit.jpg" className="img-rounded" /></center></td>
-                </tr>
-                <tr>
-                  <td className="heading"><strong>Lunch</strong></td>
-                  <td>Meatloaf<center><img src="img/planning-images/meatloaf.jpg" className="img-rounded" /></center></td>
-                  <td>Lasagna<center><img src="img/planning-images/lasagna.jpg" className="img-rounded" /></center></td>
-                  <td>Fried Rice<center><img src="img/planning-images/fried-rice.jpg" className="img-rounded" /></center></td>
-                  <td>Roast Beef<center><img src="img/planning-images/roast-beef.jpg" className="img-rounded" /></center></td>
-                  <td>Garden Burgers<center><img src="img/planning-images/garden-burger.jpg" className="img-rounded" /></center></td>
-                  <td>Thai curry<center><img src="img/planning-images/thai-curry.jpg" className="img-rounded" /></center></td>
-                  <td>Meatballs<center><img src="img/planning-images/meatballs-spaghetti.jpg" className="img-rounded" /></center></td>
-                </tr>
-                <tr>
-                  <td className="heading"><strong>Dinner</strong></td>
-                  <td>Chicken biryani<center><img src="img/planning-images/chicken-biryani.jpg" className="img-rounded" /></center></td>
-                  <td>Pumpkin ravioli<center><img src="img/planning-images/pumpkin-ravioli.jpg" className="img-rounded" /></center></td>
-                  <td>Midnight breakfast<center><img src="img/planning-images/midnight-breakfast.jpg" className="img-rounded" /></center></td>
-                  <td>Smoked salmon<center><img src="img/planning-images/smoked-salmon.jpg" className="img-rounded" /></center></td>
-                  <td>Beef chilli<center><img src="img/planning-images/beef-chilli.jpg" className="img-rounded" /></center></td>
-                  <td>Pineapple Fried Rice<center><img src="img/planning-images/pineapple-fried-rice.jpg" className="img-rounded" /></center></td>
-                  <td>Sushi<center><img src="img/planning-images/sushi.jpg" className="img-rounded" /></center></td>
-                </tr>
-                <tr>
-                  <td className="heading"><strong>Snack</strong></td>
-                  <td>Homemade granola<center><img src="img/planning-images/homemade-granola.jpg" className="img-rounded" /></center></td>
-                  <td>Cheese & crackers<center><img src="img/planning-images/cheese-crackers.jpg" className="img-rounded" /></center></td>
-                  <td>Fruit compote<center><img src="img/planning-images/fruit-compote.jpg" className="img-rounded" /></center></td>
-                  <td>Nuts<br /><center><img src="img/planning-images/nuts.jpg" className="img-rounded" /></center></td>
-                  <td>Mug cake<center><img src="img/planning-images/mug-cake.jpg" className="img-rounded" /></center></td>
-                  <td>Frozen yogurt<center><img src="img/planning-images/frozen-yogurt.jpg" className="img-rounded" /></center></td>
-                  <td>Banana smoothie<center><img src="img/planning-images/banana-smoothie.jpg" className="img-rounded" /></center></td>
-                </tr>
-              </tbody>
+              <div>
+                <CalendarEntry onPost={(postContents) => this.onPost(postContents)} />
+                    {this.state.meals.map((recipe) => {
+                      return (
+                        <CalendarEntry key={recipe.id} data={recipe} />
+                      )
+                    })}
+              </div>
             </table>
-        </container>
-      </div>
-    )
-  }
-}
+            </container>
+            </div>
+          )
+        }
+      }
