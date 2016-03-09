bc41b6282dedc6d636a9e27215c29f7ac71f1133
Stephanie Xie <sxie@umass.edu>
2016-03-09 01:11:35 -0500

    SEARCH FUCKING WORKS BOOYAH

--- app/components/searchbar.js
+++ app/components/searchbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Searchbar extends React.Component {
+export default class ResultsSearch extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -60,6 +60,6 @@ export default class Searchbar extends React.Component {
   }
 }
 
-// Searchbar.contextTypes = {
+// ResultsSearch.contextTypes = {
 //   router: React.PropTypes.object.isRequired
 // };
