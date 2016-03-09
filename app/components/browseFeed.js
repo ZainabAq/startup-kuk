d403020ccb5ef18ee26ec4cd85553a23e8c5a911
karenchau <kchau@smith.edu>
2016-03-09 02:32:16 -0500

    implemented showing favs

--- app/components/browseFeed.js
+++ app/components/browseFeed.js
@@ -48,7 +48,7 @@ export default class BrowseFeed extends React.Component {
         <ul id="categories" className="clr">
           {this.state.recipes.map((recipe, i) => {
             return (
-              <li key={i}><FeedItem info={recipe} /></li>
+              <li><FeedItem info={recipe} /></li>
             )
           })}
         </ul>
---
+++ app/components/favoritesItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+export default class FavoritesItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+console.log(this.props.data);
+    return (
+      <div className="col-md-2 thumbnail">
+        <h5 align="center"><Link to={"/recipe/" + this.props.data._id}>{this.props.data.name}</Link></h5>
+        <img src={this.props.data.img} alt={this.props.data._id} className="img-rounded croppedimg" />
+      </div>
+    );
+  }
+}
