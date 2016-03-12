3a0ec825eaffdf2b52aae57caa48c0e7a3372f6d
Emma Stephenson <estephenson@smith.edu>
2016-03-11 22:02:12 -0500

    after fixing css, recipe.js now ready for submission

--- app/components/recipe.js
+++ app/components/recipe.js
@@ -5,10 +5,6 @@ import {addFavorite} from '../server';
 import {removeFavorite} from '../server';
 import {checkUserFavorites} from '../server';
 import {addRecipeToCalendar} from '../server';
-// import {getProfileData} from '../server';
-//TODO
-//make the ratings work (this will require pulling from the database, and changing the database)
-
 export default class Recipe extends React.Component {
    constructor(props) {
       super(props);
@@ -22,14 +18,14 @@ export default class Recipe extends React.Component {
    }
 
    refresh() {
-     getRecipe(this.props.param, (recipeData) => {
-      this.setState(recipeData);
-      this.findAverageRating()
-     });
+      getRecipe(this.props.param, (recipeData) => {
+         this.setState(recipeData);
+         this.findAverageRating()
+      });
    }
 
    componentDidMount() {
-     this.refresh();
+      this.refresh();
    }
 
    /**This method loops over the list of ratings in the recipe and returns their average*/
@@ -131,107 +127,107 @@ export default class Recipe extends React.Component {
    }
 
    render() {
-      var favButtonIcon = "fa fa-heart fa-lg social-but";
+      var favButtonIcon = "fa fa-heart fa-lg";
       if (this.state.currentFavorite) {
-         favButtonIcon = "fa fa-times fa-lg social-but";
+         favButtonIcon = "fa fa-times fa-lg";
       }
       return (
          <div>
-         <div className="container recipe">
-           <div className="row">
-             <div className="col-md-1">
-             </div>
-             <div className="col-md-10">
-               <div className="panel panel-default">
-                 <div className="panel-heading2">
-                    <div className="row">
-                       <div className="col-xs-10">
-                          <h1 className="panel-title">{this.state.name}</h1>
-                          <br />
-                          <ul className="list-inline">
-                           <li className="rating">
-                              {(() => {
-                                 var elements=[];
-                                 for (var i=0; i<this.state.currentRating; i++) {
-                                    elements.push(<span className="fa fa-star fa-lg"></span>)
-                                 }
-                                 return elements;
-                              })()}
-                              {(() => {
-                                 var secondElements=[];
-                                 var lessFive = 5-this.state.currentRating;
-                                 for (var i=0; i<lessFive; i++) {
-                                    secondElements.push(<span className="fa fa-star-o fa-lg"></span>)
-                                 }
-                                 return secondElements;
-                              })()}
-                           </li>
-                           <li className="time-icons">
-                                {this.state.time}
-                              <span className="fa fa-clock-o"></span>
-                           </li>
+            <div className="container recipe">
+               <div className="row">
+                  <div className="col-md-1">
+                  </div>
+                  <div className="col-md-10">
+                     <div className="panel panel-default">
+                        <div className="panel-heading2">
+                           <div className="row">
+                              <div className="col-xs-10">
+                                 <h1 className="panel-title">{this.state.name}</h1>
+                                 <br />
+                                 <ul className="list-inline">
+                                    <li className="rating">
+                                       {(() => {
+                                          var elements=[];
+                                          for (var i=0; i<this.state.currentRating; i++) {
+                                             elements.push(<span className="fa fa-star fa-lg"></span>)
+                                          }
+                                          return elements;
+                                       })()}
+                                       {(() => {
+                                          var secondElements=[];
+                                          var lessFive = 5-this.state.currentRating;
+                                          for (var i=0; i<lessFive; i++) {
+                                             secondElements.push(<span className="fa fa-star-o fa-lg"></span>)
+                                          }
+                                          return secondElements;
+                                       })()}
+                                    </li>
+                                    <li className="time-icons">
+                                       {this.state.time}
+                                       <span className="fa fa-clock-o"></span>
+                                    </li>
 
-                          </ul>
-                       </div>
-                       <div className="col-xs-2">
-                          <ul className="list-inline pull-right">
-                           <li className="presentation">
-                              <button type="button" className="btn btn-default" onClick={(e)=>this.handleFavoriteClick(e)}><span className={favButtonIcon}></span></button>
-                           </li>
-                           <li className="presentation">
-                              <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="fa fa-calendar-check-o fa-lg"></i>
-                              </button>
+                                 </ul>
+                              </div>
+                              <div className="col-xs-2">
+                                 <ul className="list-inline pull-right">
+                                    <li className="presentation">
+                                       <button type="button" className="btn btn-default favButton" onClick={(e)=>this.handleFavoriteClick(e)}><span className={favButtonIcon}></span></button>
+                                    </li>
+                                    <li className="presentation">
+                                       <button type="button" className="btn btn-default dropdown-toggle calButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="fa fa-calendar-check-o fa-lg"></i>
+                                    </button>
 
-                              <ul className="dropdown-menu multiple">
-                          <li><a onClick={(e)=>this.handleCalendarClick(e, "Monday")}>Monday</a></li>
-                          <li><a onClick={(e)=>this.handleCalendarClick(e, "Tuesday")}>Tuesday</a></li>
-                          <li><a onClick={(e)=>this.handleCalendarClick(e, "Wednesday")}>Wednesday</a></li>
-                          <li><a onClick={(e)=>this.handleCalendarClick(e, "Thursday")}>Thursday</a></li>
-                          <li><a onClick={(e)=>this.handleCalendarClick(e, "Friday")}>Friday</a></li>
-                          <li><a onClick={(e)=>this.handleCalendarClick(e, "Saturday")}>Saturday</a></li>
-                          <li><a onClick={(e)=>this.handleCalendarClick(e, "Sunday")}>Sunday</a></li>
-                          </ul>
-                           </li>
-                          </ul>
-                       </div>
-                    </div>
-                 </div>
-                 <div className="panel-body">
+                                    <ul className="dropdown-menu multiple">
+                                       <li><a onClick={(e)=>this.handleCalendarClick(e, "Monday")}>Monday</a></li>
+                                       <li><a onClick={(e)=>this.handleCalendarClick(e, "Tuesday")}>Tuesday</a></li>
+                                       <li><a onClick={(e)=>this.handleCalendarClick(e, "Wednesday")}>Wednesday</a></li>
+                                       <li><a onClick={(e)=>this.handleCalendarClick(e, "Thursday")}>Thursday</a></li>
+                                       <li><a onClick={(e)=>this.handleCalendarClick(e, "Friday")}>Friday</a></li>
+                                       <li><a onClick={(e)=>this.handleCalendarClick(e, "Saturday")}>Saturday</a></li>
+                                       <li><a onClick={(e)=>this.handleCalendarClick(e, "Sunday")}>Sunday</a></li>
+                                    </ul>
+                                 </li>
+                              </ul>
+                           </div>
+                        </div>
+                     </div>
+                     <div className="panel-body">
 
-                    <div className="row">
-                       <div className="col-md-3 ingredient-bar">
-                          <ul>
-                             {this.state.ingredients.map((listItem, i) => {
-                                return (
-                                   <li key={i}>{listItem}</li>
-                                );
-                             })}
-                       </ul>
-                    </div>
-                 <div className="col-md-9">
-                    <img src={this.state.img} width="35%" align="right" className="img-rounded food-pic pull-right"/>
-                    <ol className="instruction-list">
-                       {this.state.instructions.map((listItem, i) => {
-                         return (
-                            <li key={i}>{listItem}</li>
-                         );
-                       })}
-                    </ol>
-                 </div>
-             </div>
+                        <div className="row">
+                           <div className="col-md-3 ingredient-bar">
+                              <ul>
+                                 {this.state.ingredients.map((listItem, i) => {
+                                    return (
+                                       <li key={i}>{listItem}</li>
+                                    );
+                                 })}
+                              </ul>
+                           </div>
+                           <div className="col-md-9">
+                              <img src={this.state.img} width="35%" align="right" className="img-rounded food-pic pull-right"/>
+                              <ol className="instruction-list">
+                                 {this.state.instructions.map((listItem, i) => {
+                                    return (
+                                       <li key={i}>{listItem}</li>
+                                    );
+                                 })}
+                              </ol>
+                           </div>
+                        </div>
 
-                 </div>
+                     </div>
+                  </div>
+
                </div>
-
-             </div>
-             <div className="col-md-1">
-             </div>
-           </div>
+               <div className="col-md-1">
+               </div>
+            </div>
          </div>
-         </div>
+      </div>
 
 
-      )
-   }
+   )
+}
 
 }
