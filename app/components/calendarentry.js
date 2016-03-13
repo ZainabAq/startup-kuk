ee3e228f1a8dd0b2410a70bf6b882644e3d59106
karenchau <kchau@smith.edu>
2016-03-13 16:22:04 -0400

    added comments to favorites page and favoritesitem

--- app/components/calendarentry.js
+++ app/components/calendarentry.js
@@ -21,7 +21,7 @@ onRemove(e, id) {
         <li>
           <div className="col-md-10 ">
             <h5 className="list-group-item-heading text-center">{this.props.type}</h5>
-            <Link to={"/recipe/" + this.props.data._id} className="list-group-item-text"><h6 className="text-center">{this.props.data.name}</h6>
+            <Link to={"/recipe/" + this.props.data._id} className="list-group-item-text"><h6 className="text-center font1">{this.props.data.name}</h6>
             <img src={this.props.data.img} alt={this.props.data._id} className="img-rounded croppedimg" /></Link>
             <button className="btn-sm btn-transparent" type="button" onClick={(e) =>this.onRemove(e, this.props.data._id)}><span className="glyphicon glyphicon-remove onImage"></span></button>
             </div>
