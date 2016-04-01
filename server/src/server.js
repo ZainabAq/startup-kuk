e41f4db109c4dfb371f45d1862ae50a1b2aebe81
Zainab Aqdas <srizvi@smith.edu>
2016-04-01 09:41:39 -0400

    Server up and running

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,18 @@
 // Implement your server in this file.
 // We should be able to run your server with node src/server.js
+// Imports the express Node module.
+
+var express = require('express');
+// Creates an Express server.
+var app = express();
+
+app.use(express.static('../client/build'));
+
+
+
+
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
