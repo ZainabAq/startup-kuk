f295f5077fb8fcc11b393ee0c8754e88f8f8aca7
Zainab Aqdas <srizvi@smith.edu>
2016-04-04 21:18:31 -0400

    Added the error msg from workshop 6 for debugging purposes

--- client/app/server.js
+++ client/app/server.js
@@ -44,7 +44,7 @@ function getUpcomingMeals(userId) {
   // For now, static date is Monday.
   var meals = [];
   calendar.Monday.forEach((recipeId) => {
-    meals.push(getRecipeSync(recipeId));
+    meals.push(get(recipeId));
   })
   return meals;
 }
@@ -55,16 +55,15 @@ function getUpcomingMeals(userId) {
 * @returns A 4-element array of that day's meals
 */
 
-function getCalendarData(userId, week, day) {
-  //var userData = readDocument('users', userId);
-  var calendar = readDocument('calendar', week);
-  var meals = [];
-  calendar[day].forEach((recipeId) => {
-    meals.push(getRecipeSync(recipeId));
-  })
-  return meals;
-
-}
+// function getCalendarData(userId, week, day) {
+//   //var userData = readDocument('users', userId);
+//   var calendar = readDocument('calendar', week);
+//   var meals = [];
+//   calendar[day].forEach((recipeId) => {
+//     meals.push(getRecipeSync(recipeId));
+//   })
+//   return meals;
+// }
 
 function removeRecipefromCalendarhere(id, week, day, i) {
   var calendar = readDocument('calendar', week);
@@ -108,19 +107,28 @@ export function getProfileData(user, cb) {
 }
 
 export function getProfileCalendarData(user, week, cb) {
-  // Get the User object with the id "user".
-  var userData = readDocument('users', user);
-  // Add upcoming calendar
-  userData.Monday = getCalendarData(user, week, "Monday");
-  userData.Tuesday = getCalendarData(user, week, "Tuesday");
-  userData.Wednesday = getCalendarData(user, week, "Wednesday");
-  userData.Thursday = getCalendarData(user, week, "Thursday");
-  userData.Friday = getCalendarData(user, week, "Friday");
-  userData.Saturday = getCalendarData(user, week, "Saturday");
-  userData.Sunday = getCalendarData(user, week, "Sunday");
-  // Return UserData with resolved references.
-  emulateServerReturn(userData, cb);
-}
+  sendXHR('GET', '/calendar/' + week, undefined, (xhr) => {
+        // Call the callback with the data.
+        cb(JSON.parse(xhr.responseText));
+      });
+  }
+
+
+
+// export function getProfileCalendarData(user, week, cb) {
+//   // Get the User object with the id "user".
+//   var userData = readDocument('users', user);
+//   // Add upcoming calendar
+//   userData.Monday = getCalendarData(user, week, "Monday");
+//   userData.Tuesday = getCalendarData(user, week, "Tuesday");
+//   userData.Wednesday = getCalendarData(user, week, "Wednesday");
+//   userData.Thursday = getCalendarData(user, week, "Thursday");
+//   userData.Friday = getCalendarData(user, week, "Friday");
+//   userData.Saturday = getCalendarData(user, week, "Saturday");
+//   userData.Sunday = getCalendarData(user, week, "Sunday");
+//   // Return UserData with resolved references.
+//   emulateServerReturn(userData, cb);
+// }
 
 //need functions to addFavorites, addRating, addMealstoCalendar, getRecipeInformation
 //modifyRestrictions (for the profile)
@@ -405,3 +413,68 @@ export function findRecipeByIngredients(ingredientsList, cb) {
 }
 
 //   XHR REQUEST MAIN CODE (from Workshop 6)
+var token = 'eyJpZCI6NH0='; // <-- Put your base64'd JSON token here
+/**
+ * Properly configure+send an XMLHttpRequest with error handling, authorization token,
+ * and other needed properties.
+ */
+function sendXHR(verb, resource, body, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(verb, resource);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+
+  // The below comment tells ESLint that FacebookError is a global.
+  // Otherwise, ESLint would complain about it! (See what happens in Atom if
+  // you remove the comment...)
+  /*global FacebookError*/
+
+  // Response received from server. It could be a failure, though!
+  xhr.addEventListener('load', function() {
+    var statusCode = xhr.status;
+    var statusText = xhr.statusText;
+    if (statusCode >= 200 && statusCode < 300) {
+      // Success: Status code is in the [200, 300) range.
+      // Call the callback with the final XHR object.
+      cb(xhr);
+    } else {
+      // Client or server error.
+      // The server may have included some response text with details concerning
+      // the error.
+      var responseText = xhr.responseText;
+      FacebookError('Could not ' + verb + " " + resource + ": Received " + statusCode + " " + statusText + ": " + responseText);
+    }
+  });
+
+  // Time out the request if it takes longer than 10,000 milliseconds (10 seconds)
+  xhr.timeout = 10000;
+
+  // Network failure: Could not connect to server.
+  xhr.addEventListener('error', function() {
+    FacebookError('Could not ' + verb + " " + resource + ": Could not connect to the server.");
+  });
+
+  // Network failure: request took too long to complete.
+  xhr.addEventListener('timeout', function() {
+    FacebookError('Could not ' + verb + " " + resource + ": Request timed out.");
+  });
+
+  switch (typeof(body)) {
+    case 'undefined':
+      // No body to send.
+      xhr.send();
+      break;
+    case 'string':
+      // Tell the server we are sending text.
+      xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+      xhr.send(body);
+      break;
+    case 'object':
+      // Tell the server we are sending JSON.
+      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      // Convert body into a JSON string.
+      xhr.send(JSON.stringify(body));
+      break;
+    default:
+      throw new Error('Unknown body type: ' + typeof(body));
+  }
+}
--- server/src/server.js
+++ server/src/server.js
@@ -24,6 +24,31 @@ function getRecipeSync(recipeId) {
    return recipe;
 }
 
+function getCalendarSync(userId, week, day) {
+  var calendar = readDocument('calendar', week);
+  var meals = [];
+  calendar[day].forEach((recipeId) => {
+    meals.push(getRecipeSync(recipeId));
+  })
+  return meals;
+
+}
+
+// Get ProfileCalendarData
+app.get('calendar/:week', function(req, res) {
+  var week = req.params.week;
+  var user = req.params.user;
+  var userData = readDocument('users', user);
+  userData.Monday = getCalendarSync(user, week, "Monday");
+  userData.Tuesday = getCalendarSync(user, week, "Tuesday");
+  userData.Wednesday = getCalendarSync(user, week, "Wednesday");
+  userData.Thursday = getCalendarSync(user, week, "Thursday");
+  userData.Friday = getCalendarSync(user, week, "Friday");
+  userData.Saturday = getCalendarSync(user, week, "Saturday");
+  userData.Sunday = getCalendarSync(user, week, "Sunday");
+  res.send();
+});
+
 app.get('/recipe/:recipeid/', function(req, res) {
    //get the recipe id out of the url
    var recipeid = req.params.recipeid;
